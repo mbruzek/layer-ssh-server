f55894d6c48688fcd528f61828672e9ab180b02c
Matt Bruzek <matthew.bruzek@canonical.com>
2016-08-29 23:15:07 -0500

    Update to the reactive code.

--- reactive/ssh_server.sh
+++ reactive/ssh_server.sh
@@ -8,13 +8,20 @@ source charms.reactive.sh
 function install_ssh-server() {
     apt-get install openssh-server
     charms.reactive set_state 'ssh-server.installed'
+    status-set active "The ssh-server software is installed."
 }
 
 @when 'ssh-server.installed'
 @when 'config.changed.port'
 function configure_port() {
     local port=`config-get port`
-    status-set maintenance "Configuring sshd for ${port}"
+    status-set maintenance "Configuring ssh-server for ${port}"
+    local old_port=`grep -e "^Port " /etc/ssh/sshd_config | head -n 1 | cut -d ' ' -f 2`
+    close-port $old_port || true
+    # The /etc/ssh/sshd_config file contains keyword-argument pairs, one per 
+    # line. Lines starting  with ‘#’ and empty lines are interpreted as 
+    # comments. Arguments may optionally be enclosed in double quotes (") in
+    # order to represent arguments containing spaces.
     sed -i -e "s/^Port .*$/Port ${port}/" /etc/ssh/sshd_config 
     systemctl reload sshd 
     systemctl status sshd
